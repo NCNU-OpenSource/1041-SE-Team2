d6f1ffcd34c0ed40986fcdb21de4cd68343b7c73
YYSU <jim123820@yahoo.com.tw>
2015-12-25 21:16:28 +0800

    finish store function

--- game.php
+++ game.php
@@ -13,6 +13,7 @@ if($rs=mysqli_fetch_array($result)){
     $money=$rs['Money'];
     $exp=$rs['Exp'];
     $level=$rs['Level'];
+    $id=$rs['Id'];
 }
 
 $sql2="select Exp from Level where Lev=$level+1";
@@ -20,7 +21,6 @@ $results=mysqli_query($conn,$sql2);
 if($rs2=mysqli_fetch_array($results)){
     $experience=$rs2['Exp'];
 }
-
 
 echo "<script>var exp1=".$exp/$experience."*100;var exp=exp1+\"%\";</script>";  //傳值給javascript
 
@@ -52,7 +52,7 @@ echo "<script>var exp1=".$exp/$experience."*100;var exp=exp1+\"%\";</script>";  
     		position:absolute;
     		left:50%;
 			top:50%;
-			margin-top:-260px;
+			margin-top:-270px;
 			margin-left:-280px;
     		border:solid;
             max-width:100px;
@@ -69,7 +69,7 @@ echo "<script>var exp1=".$exp/$experience."*100;var exp=exp1+\"%\";</script>";  
             position:absolute;
             left:50%;
             top:50%;
-            margin-top:-260px;
+            margin-top:-270px;
             margin-left:-370px;       
         }
         .money{
@@ -131,20 +131,34 @@ echo "<script>var exp1=".$exp/$experience."*100;var exp=exp1+\"%\";</script>";  
     <div class="modal fade" id="storeModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
+
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
             <br/>
           </div>
-          <div class="modal-body">
-            <img src="material/oven.png" alt="烤箱數量" class="img-thumbnail">
 
-            <hr/>
-            <img src="material/food_bag.png" alt="材料包數量" class="img-thumbnail">            
+          <div class="modal-body">
+            <form class="form-inline" role="form">
+                <div class="form-group">&nbsp;&nbsp;&nbsp;&nbsp;
+                    <img src="material/oven.png" alt="烤箱數量" class="img-thumbnail">
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="glyphicon glyphicon-chevron-left" id="o_left"></span>&nbsp;
+                    <input type="text" class="form-control" value="0" id="oven" style="width:100px">&nbsp;
+                    <span class="glyphicon glyphicon-chevron-right" id="o_right"></span>&nbsp;
+                    <button type="button" class="btn btn-default" id="o_buy">購買</button>  <br/>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="glyphicon glyphicon-usd" id="Addmoney"></span> 1000          
+                </div>
+                    <hr/>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <img src="material/food_bag.png" alt="材料包數量" class="img-thumbnail">   
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="glyphicon glyphicon-chevron-left" id="p_left"></span>&nbsp;
+                    <input type="text" class="form-control" value="0" id="package" style="width:100px">&nbsp;
+                    <span class="glyphicon glyphicon-chevron-right" id="p_right"></span>&nbsp;
+                    <button type="button" class="btn btn-default" id="p_buy">購買</button>  <br/>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="glyphicon glyphicon-usd" id="Addmoney"></span> 100       
+                 
           </div>
-          <!-- <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div> -->
+          </form>
         </div>
       </div>
     </div>
@@ -155,9 +169,89 @@ echo "<script>var exp1=".$exp/$experience."*100;var exp=exp1+\"%\";</script>";  
 <script>
 $(document).ready(function() {
    $('.progress-bar').css("width",exp);  //產生經驗值條
+   //烤箱箭頭
+   $('#o_right').on("click",oadd);
+   $('#o_left').on('click',osubtract);
+   //材料包箭頭
+   $('#p_right').on("click",padd);
+   $('#p_left').on('click',psubtract);
+   //登出
+    $('#logout').on("click",out);
+    
 });
- $('#logout').on("click",out);
 function out(){ 
     window.location.href="logout.php";
 }
+function oadd(){
+    var value=$('#oven').val();
+    value++;
+    $('#oven').val(value);
+}
+function osubtract(){
+    var value=$('#oven').val();
+    value--;
+    if(value<=0){
+        $('#oven').val(0);
+    }
+    else{
+        $('#oven').val(value);
+    }
+}
+function padd(){
+    var value=$('#package').val();
+    value++;
+    $('#package').val(value);
+}
+function psubtract(){
+    var value=$('#package').val();
+    value--;
+    if(value<=0){
+        $('#package').val(0);
+    }
+    else{
+        $('#package').val(value);
+    }
+}
+// 烤箱購買
+$('#o_buy').on("click",function (){
+    var buy = $('#oven').val();
+    $.ajax({
+            url: 'buy.php',
+            dataType: 'html',
+            type: 'POST',
+            data: { 
+                Oven_num: buy,
+                Id:'<?php echo $rs['Id'] ?>'
+            },
+            error: function(xhr) {
+               // $('#'+DIV).html(xhr);
+                },
+            success: function(response) {
+                //$('#'+DIV).html(response); //set the html content of the object msg
+                $('.money').empty();
+                $('.money').append("<span class='glyphicon glyphicon-usd' id='Addmoney'></span>").append(response);
+            }
+        });
+});
+// 材料包購買
+$('#p_buy').on("click",function (){
+    var buy = $('#package').val();
+    $.ajax({
+            url: 'buy.php',
+            dataType: 'html',
+            type: 'POST',
+            data: { 
+                Package: buy,
+                Id:'<?php echo $rs['Id'] ?>'
+            },
+            error: function(xhr) {
+               // $('#'+DIV).html(xhr);
+                },
+            success: function(response) {
+                //$('#'+DIV).html(response); //set the html content of the object msg
+                $('.money').empty();
+                $('.money').append("<span class='glyphicon glyphicon-usd' id='Addmoney'></span>").append(response);
+            }
+        });
+});
 </script>
