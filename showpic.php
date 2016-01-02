52b33c57fcd6b64c7c50db3dae16f5ba3dbdb4e1
102213015 <alan831221@gmail.com>
2016-01-03 00:33:33 +0800

    大頭貼上傳Mysql

---
+++ showpic.php
@@ -0,0 +1,21 @@
+<?php
+    
+    require('dbconn.php');
+    
+    $SQLSTR="select * from myimage where filename='"
+         . $_REQUEST["filename"] . "'";
+	$result = mysqli_query($conn,$SQLSTR);
+	
+	if($rs = mysqli_fetch_array($result)){
+		$ph = $rs['filetype'];
+	}
+	
+	header("Content-Type: $ph");//設定網頁資料格式
+	
+	echo base64_decode($rs['filepic']);// 輸出圖片資料
+	echo $rs['filename'];
+	
+	
+	
+?>
+    
--- upload.php
+++ upload.php
@@ -2,25 +2,56 @@
 
     //取得上傳檔案資訊
 
-    $filename = $_FILES['image']['name'];
+    $filename = $_FILES['upfile']['name'];
 
-    $tmpname = $_FILES['image']['tmp_name'];
+    $tmpname = $_FILES['upfile']['tmp_name'];
 
-    $filetype = $_FILES['image']['type'];
+    $filetype = $_FILES['upfile']['type'];
 
-    $filesize = $_FILES['image']['size'];    
+    $filesize = $_FILES['upfile']['size'];    
 
     $file = NULL;
-
-    echo "$filename,$tmpname,$filetype,$filesize,$file";
 	
-    if(isset($_FILES['image']['error'])){    
+	if($filesize > 80000){
+		header("location:upload.html");
+	}
 
-        if($_FILES['image']['error']==0){                                 
-            $instr = fopen($tmpname,"rb" );
-            $file = addslashes(fread($instr,filesize($tmpname)));
+    if ( $_FILES["upfile"]["size"] > 0 )
+        {
+         //開啟圖片檔
+         $file = fopen($_FILES["upfile"]["tmp_name"], "rb");
+         // 讀入圖片檔資料
+         $fileContents = fread($file, filesize($_FILES["upfile"]["tmp_name"]));
+         //關閉圖片檔
+         fclose($file);
+
+         // 圖片檔案資料編碼
+         $fileContents = base64_encode($fileContents);
+
+         //連結MySQL Server
+         $conn = mysqli_connect("localhost","myid","12345","kitchen");
+         mysqli_query($conn,"SET NAMES utf8");
+         //組合查詢字串
+         $SQLSTR="Insert into myimage (filename,filesize,filetype,filepic) values('"
+                  . $_FILES["upfile"]["name"] . "',"
+                  . $_FILES["upfile"]["size"] . ",'"
+                  . $_FILES["upfile"]["type"] . "','"
+                  . $fileContents . "')";
+         //將圖片檔案資料寫入資料庫
+		 
+         if(!mysqli_query($conn,$SQLSTR)==0)
+           {
+            echo "您所上傳的檔案已儲存進入資料庫<a href=\"showpic.php?filename="
+                 . $_FILES["upfile"]["name"] . "\">"
+                 . $_FILES["upfile"]["name"] . "</a>";
+           }
+         else
+           {
+            echo "您所上傳的檔案無法儲存進入資料庫";
+           }
         }
-	}	
-	
-	move_uploaded_file($_FILES["image"]["tmp_name"],"upload/".$_FILES["image"]["name"]);
+      else
+        {
+         echo "圖片上傳失敗";
+        }
 ?>
